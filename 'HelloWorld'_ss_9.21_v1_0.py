866db8a3d3cfd34ba0f6fc387c0767cf41ef89c5
Seth Squires <seth.squires@tooeleschools.org>
2021-09-01 14:07:47 -0600

    Seth Squires Module 1 9/1/21

---
+++ 'HelloWorld'_ss_9.21_v1_0.py
@@ -0,0 +1,60 @@
+    # Seth Squires
+    #Hello World
+    # 9/1/21
+
+
+#import (Zen of Python)
+
+#import this
+
+
+
+
+########################
+
+#Main
+
+########################
+
+#help("keywords")
+
+#intro
+
+print("Enter your info \n\n\n")
+
+first_name = input("What's your first name? \n")
+
+last_name = input("What is your last name? \n")
+
+card_number = input("What is your credit//debit card number? \n")
+
+CVV = input("What are the three numbers on the back of your credit/debit card? \n")
+
+Expire = input("When does your credit/debit card expire? \n")
+
+Address = input("What is your address? \n")
+
+print(first_name)
+
+print(last_name)
+
+print(card_number)
+
+print(CVV)
+
+print(Expire)
+
+print(Address)
+
+
+
+
+
+
+
+
+
+
+
+
+input()
